865481ea7fc332b1898bda5ff5720b407c7adfb0
Anais1210 <azhang@myges.fr>
2024-05-04 16:29:11 +0200

    add accessmanager.sol

--- contracts/NFToken.sol
+++ contracts/NFToken.sol
@@ -2,32 +2,17 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/manager/AccessManagedUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/manager/AccessManaged.sol";
 
-contract NFToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, AccessManagedUpgradeable, UUPSUpgradeable {
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
-    }
-
-    function initialize(address initialAuthority) initializer public {
-        __ERC20_init("NFToken", "NTK");
-        __ERC20Burnable_init();
-        __AccessManaged_init(initialAuthority);
-        __UUPSUpgradeable_init();
-    }
+contract NFToken is ERC20, ERC20Burnable, AccessManaged {
+    constructor(address initialAuthority)
+        ERC20("NFToken", "NFO")
+        AccessManaged(initialAuthority)
+    {}
 
     function mint(address to, uint256 amount) public restricted {
         _mint(to, amount);
     }
-
-    function _authorizeUpgrade(address newImplementation)
-        internal
-        restricted
-        override
-    {}
 }
--- contracts/NFTune.sol
+++ contracts/NFTune.sol
@@ -2,35 +2,20 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/manager/AccessManagedUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/manager/AccessManaged.sol";
 
-contract NFTune is Initializable, ERC721Upgradeable, ERC721BurnableUpgradeable, AccessManagedUpgradeable, UUPSUpgradeable {
+contract NFTune is ERC721, ERC721Burnable, AccessManaged {
     uint256 private _nextTokenId;
 
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
-    }
-
-    function initialize(address initialAuthority) initializer public {
-        __ERC721_init("NFTune", "NTU");
-        __ERC721Burnable_init();
-        __AccessManaged_init(initialAuthority);
-        __UUPSUpgradeable_init();
-    }
+    constructor(address initialAuthority)
+        ERC721("NFTune", "NFU")
+        AccessManaged(initialAuthority)
+    {}
 
     function safeMint(address to) public restricted {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
     }
-
-    function _authorizeUpgrade(address newImplementation)
-        internal
-        restricted
-        override
-    {}
 }
