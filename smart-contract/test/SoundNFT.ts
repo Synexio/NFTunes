a861af5b4dfdca6b09510b6ff9a8818eabfd08c3
Anais1210 <azhang@myges.fr>
2024-08-10 00:38:38 +0800

    test hardhat mint artist ok

--- smart-contract/test/SoundNFT.ts
+++ smart-contract/test/SoundNFT.ts
@@ -1,8 +1,9 @@
 import { expect } from "chai";
 import hre, { ethers, upgrades } from "hardhat";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 
 describe("SoundNFT", function () {
-  it("should return name", async function () {
+  async function deployNFTuneContract() {
     const [owner, admin, artist, addr1, addr2] = await ethers.getSigners();
     const SoundNFT = await hre.ethers.getContractFactory("SoundNFT");
     const soundNFT = await upgrades.deployProxy(
@@ -10,9 +11,22 @@ describe("SoundNFT", function () {
       [admin.address, artist.address, "SoundNFT", "SNFT"],
       { initializer: "initialize" }
     );
+    return { soundNFT, admin, artist, addr1, addr2 };
+  }
+  it("should return nft's name", async function () {
+    const { soundNFT, admin } = await loadFixture(deployNFTuneContract);
 
     await soundNFT.waitForDeployment();
     console.log(await soundNFT.name());
     expect(await soundNFT.name()).to.equal("SoundNFT");
+  });
+  it("should mint nft", async function () {
+    const { soundNFT, admin, artist } = await loadFixture(deployNFTuneContract);
+    await soundNFT
+      .connect(artist)
+      .safeMint(artist.address, `ipfs://tokenUri${0}`);
+    soundNFT.connect(artist);
+    expect(await soundNFT.balanceOf(artist.address)).to.equal(1);
+    expect(await soundNFT.tokenURI(0)).to.equal(`ipfs://tokenUri${0}`);
   });
 });
