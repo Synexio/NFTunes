c1efb8e3de8aa84883c32bce01d5930ad36e4825
Anais1210 <azhang@myges.fr>
2024-08-19 15:24:55 +0200

    test hardhat sound token

---
+++ smart-contract/test/AlbumFactory.ts
@@ -0,0 +1,34 @@
+import hre, { upgrades, ethers } from "hardhat";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+
+describe("Album Factory", function () {
+  async function deployNFTFactoryContract() {
+    const [owner, admin, artist, addr1, addr2] = await ethers.getSigners();
+    const SoundFactory = await hre.ethers.getContractFactory("SoundNFTFactory");
+    const soundFactory = await upgrades.deployProxy(
+      SoundFactory,
+      [admin.address, artist.address],
+      {
+        initializer: "initialize",
+      }
+    );
+
+    return { soundFactory, owner, admin, artist, addr1, addr2 };
+  }
+
+  it("should create an album", async function () {
+    const { soundFactory, owner, artist, admin } = await loadFixture(
+      deployNFTFactoryContract
+    );
+    const tx = await soundFactory
+      .connect(artist)
+      .createAlbum("album1", "abl1", admin.address, artist.address);
+    soundFactory.connect(artist);
+    const receipt = await tx.wait();
+    expect(receipt.status).to.equal(1);
+    const deployedAlbumAddress = await soundFactory.deployedAlbums(0);
+
+    console.log("New Album Address:", deployedAlbumAddress);
+  });
+});
---
+++ smart-contract/test/SoundToken.ts
@@ -0,0 +1,26 @@
+import { expect } from "chai";
+import hre, { ethers, upgrades } from "hardhat";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+
+describe("SoundToken", function () {
+  async function deployNFTuneContract() {
+    const [owner, admin, artist, addr1, addr2] = await ethers.getSigners();
+    const SoundToken = await hre.ethers.getContractFactory("SoundToken");
+    const soundToken = await upgrades.deployProxy(SoundToken, [admin.address], {
+      initializer: "initialize",
+    });
+    return { soundToken, admin, artist, addr1, addr2 };
+  }
+
+  it("should mint nft", async function () {
+    const { soundToken, admin } = await loadFixture(deployNFTuneContract);
+    await soundToken.connect(admin).mint(admin.address, 100000);
+    soundToken.connect(admin);
+    expect(await soundToken.balanceOf(admin.address)).to.equal(100000);
+  });
+  it("should not mint", async function () {
+    const { soundToken, addr1 } = await loadFixture(deployNFTuneContract);
+    await expect(soundToken.connect(addr1).mint(addr1.address, 1000)).to.be
+      .reverted;
+  });
+});
